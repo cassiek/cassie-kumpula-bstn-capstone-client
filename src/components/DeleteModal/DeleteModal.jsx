9c4dcfb3dd77bd27eed9ec8bb38a29018450bb4d
cassiek <ckumpula123@gmail.com>
2024-10-29 23:30:01 -0400

    update delete modal styling

--- src/components/DeleteModal/DeleteModal.jsx
+++ src/components/DeleteModal/DeleteModal.jsx
@@ -4,6 +4,7 @@ import Modal from "react-modal";
 import React from "react";
 import { deleteWarehouse } from "../../services/warehouses-api.js";
 import { deleteItem } from "../../services/inventory-i-api";
+import "./DeleteModal.scss";
 
 function DeleteModal({ isOpen, closeModal, id, type }) {
     Modal.setAppElement("#root");
@@ -25,15 +26,16 @@ function DeleteModal({ isOpen, closeModal, id, type }) {
 
     return (
         <Modal
+            className="modal-container"
             isOpen={isOpen}
             onRequestClose={closeModal}
             contentLabel="Delete Modal"
         >
-            <div>
-                <p>Are you sure you want to delete?</p>
-                <div>
-                    <button onClick={closeModal}>CANCEL</button>
-                    <button onClick={deleteSelected} onBlur={closeModal}>DELETE</button>
+            <div className="modal">
+                <h2 className="modal__paragraph">Are you sure you want to delete this {type ? "warehouse" : "inventory"}?</h2>
+                <div className="modal__buttons">
+                    <button className="modal__button" onClick={closeModal}>CANCEL</button>
+                    <button className="modal__button" onClick={deleteSelected} onBlur={closeModal}>DELETE</button>
                 </div>
             </div>
         </Modal>
